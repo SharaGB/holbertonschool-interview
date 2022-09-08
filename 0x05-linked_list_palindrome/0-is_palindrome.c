93838c9b1330e7268f0e320c6f3ff7b2c4da2cac
SharaGB <shara.garcia.betancur@gmail.com>
2022-09-08 15:48:01 -0500

    Function that checks if a singly linked list is a palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,27 +1,41 @@
 #include "lists.h"
 /**
+ * recursive_palindrome - function to check if a linked list is a palindrome
+ * @head: head of the linked list
+ * @current: current node
+ *
+ * Return: pointer to something if palindrome, else NULL
+ */
+listint_t *recursive_palindrome(listint_t *current, listint_t *head)
+{
+	listint_t *next = NULL;
+
+	if (current == NULL)
+		return (head);
+	next = recursive_palindrome(current->next, head);
+	if (next == NULL)
+		return (NULL);
+	if (next->n == current->n)
+	{
+		if (current == head)
+			return (current);
+		return (next->next);
+	}
+	return (NULL);
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: double pointer to head of list
+ *
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *slow_ptr = *head, *fast_ptr = *head;
-	int i = 0;
-
 	if (head == NULL || *head == NULL)
 		return (1);
 
-	if (*head != NULL && (*head)->next != NULL)
-	{
-		while (fast_ptr != NULL && fast_ptr->next != NULL)
-		{
-			if (slow_ptr->n != fast_ptr->next->n)
-			{
-				return (1);
-			}
-			i++;
-		}
-	}
-	return (0);
+	if (recursive_palindrome(*head, *head) == NULL)
+		return (0);
+	return (1);
 }
