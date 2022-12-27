8c78c6233cc19d82360da51782edcc5efaa5d399
SharaGB <shara.garcia.betancur@gmail.com>
2022-12-26 21:00:04 -0500

    Inserts a value into a Max Binary Heap

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -20,12 +20,10 @@ typedef struct binary_tree_s
 	struct binary_tree_s *right;
 } binary_tree_t;
 
-/* Print function */
-void binary_tree_print(const binary_tree_t *);
-
 typedef struct binary_tree_s heap_t;
 
-/* Task prototypes */
+void binary_tree_print(const binary_tree_t *);
+heap_t *heap_insert(heap_t **root, int value);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */
