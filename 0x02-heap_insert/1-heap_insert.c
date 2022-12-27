e8d8d59a0f3c1f09c7317ce216e4a9be34a2615b
SharaGB <shara.garcia.betancur@gmail.com>
2022-12-26 21:04:47 -0500

    Inserts a value into a Max Binary Heap

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -6,19 +6,19 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    size_t depthl = 0;
-    size_t depthr = 0;
+	size_t depthl = 0;
+	size_t depthr = 0;
 
-    if (tree == NULL)
-    {
-        return (0);
-    }
-    else
-    {
-        depthl = binary_tree_size(tree->left);
-        depthr = binary_tree_size(tree->right);
-        return ((depthl + depthr) + 1);
-    }
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		depthl = binary_tree_size(tree->left);
+		depthr = binary_tree_size(tree->right);
+		return ((depthl + depthr) + 1);
+	}
 }
 
 /**
@@ -28,23 +28,23 @@ size_t binary_tree_size(const binary_tree_t *tree)
  */
 heap_t *inipoint(heap_t *star)
 {
-    int n_tmp = 0;
+	int n_tmp = 0;
 
-    if (!star->parent || !star)
-        return (star);
+	if (!star->parent || !star)
+		return (star);
 
-    while (star->parent)
-    {
-        if (star->n < star->parent->n)
-        {
-            return (star);
-        }
-        n_tmp = star->n;
-        star->n = star->parent->n;
-        star->parent->n = n_tmp;
-        star = star->parent;
-    }
-    return (star);
+	while (star->parent)
+	{
+		if (star->n < star->parent->n)
+		{
+			return (star);
+		}
+		n_tmp = star->n;
+		star->n = star->parent->n;
+		star->parent->n = n_tmp;
+		star = star->parent;
+	}
+	return (star);
 }
 
 /**
@@ -55,44 +55,44 @@ heap_t *inipoint(heap_t *star)
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    int tmp_mk = 0;
-    int idx_b = 0;
-    ssize_t size_t = (1 + binary_tree_size(*root));
-    heap_t *tmp_node = *root;
-    heap_t *tmp_n_node = NULL;
+	int tmp_mk = 0;
+	int idx_b = 0;
+	ssize_t size_t = (1 + binary_tree_size(*root));
+	heap_t *tmp_node = *root;
+	heap_t *tmp_n_node = NULL;
 
-    tmp_n_node = binary_tree_node(tmp_node, value);
-    if (!tmp_n_node)
-    {
-        return (NULL);
-    }
-    if (!*root)
-    {
-        return (*root = tmp_n_node);
-    }
-    for (;;)
-    {
-        if (1 << (idx_b + 1) > size_t)
-            break;
-        idx_b++;
-    }
-    idx_b--;
-    for (; idx_b > 0; idx_b--)
-    {
-        tmp_mk = (1 << idx_b);
-        if (size_t & tmp_mk)
-        {
-            tmp_node = tmp_node->right;
-        }
-        else
-        {
-            tmp_node = tmp_node->left;
-        }
-    }
-    if (size_t & 1)
-        tmp_node->right = tmp_n_node;
-    else
-        tmp_node->left = tmp_n_node;
-    tmp_n_node->parent = tmp_node;
-    return (inipoint(tmp_n_node));
+	tmp_n_node = binary_tree_node(tmp_node, value);
+	if (!tmp_n_node)
+	{
+		return (NULL);
+	}
+	if (!*root)
+	{
+		return (*root = tmp_n_node);
+	}
+	for (;;)
+	{
+		if (1 << (idx_b + 1) > size_t)
+			break;
+		idx_b++;
+	}
+	idx_b--;
+	for (; idx_b > 0; idx_b--)
+	{
+		tmp_mk = (1 << idx_b);
+		if (size_t & tmp_mk)
+		{
+			tmp_node = tmp_node->right;
+		}
+		else
+		{
+			tmp_node = tmp_node->left;
+		}
+	}
+	if (size_t & 1)
+		tmp_node->right = tmp_n_node;
+	else
+		tmp_node->left = tmp_n_node;
+	tmp_n_node->parent = tmp_node;
+	return (inipoint(tmp_n_node));
 }
--- 0x02-heap_insert/1-main.c
+++ 0x02-heap_insert/1-main.c
@@ -9,12 +9,12 @@
  */
 static void _binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree)
-    {
-        _binary_tree_delete(tree->left);
-        _binary_tree_delete(tree->right);
-        free(tree);
-    }
+	if (tree)
+	{
+		_binary_tree_delete(tree->left);
+		_binary_tree_delete(tree->right);
+		free(tree);
+	}
 }
 
 /**
@@ -24,34 +24,34 @@ static void _binary_tree_delete(binary_tree_t *tree)
  */
 int main(void)
 {
-    heap_t *root;
-    heap_t *node;
+	heap_t *root;
+	heap_t *node;
 
-    root = NULL;
-    node = heap_insert(&root, 98);
-    printf("Inserted: %d\n", node->n);
-    binary_tree_print(root);
-    node = heap_insert(&root, 402);
-    printf("\nInserted: %d\n", node->n);
-    binary_tree_print(root);
-    node = heap_insert(&root, 12);
-    printf("\nInserted: %d\n", node->n);
-    binary_tree_print(root);
-    node = heap_insert(&root, 46);
-    printf("\nInserted: %d\n", node->n);
-    binary_tree_print(root);
-    node = heap_insert(&root, 128);
-    printf("\nInserted: %d\n", node->n);
-    binary_tree_print(root);
-    node = heap_insert(&root, 256);
-    printf("\nInserted: %d\n", node->n);
-    binary_tree_print(root);
-    node = heap_insert(&root, 512);
-    printf("\nInserted: %d\n", node->n);
-    binary_tree_print(root);
-    node = heap_insert(&root, 50);
-    printf("\nInserted: %d\n", node->n);
-    binary_tree_print(root);
-    _binary_tree_delete(root);
-    return (0);
+	root = NULL;
+	node = heap_insert(&root, 98);
+	printf("Inserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 402);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 12);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 46);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 128);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 256);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 512);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	node = heap_insert(&root, 50);
+	printf("\nInserted: %d\n", node->n);
+	binary_tree_print(root);
+	_binary_tree_delete(root);
+	return (0);
 }
