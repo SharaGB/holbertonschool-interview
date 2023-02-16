c0bb482401cb408109ec980412a586f9aecceec5
SharaGB <shara.garcia.betancur@gmail.com>
2023-02-16 11:10:15 -0500

    UTF-8 Validation

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -3,9 +3,8 @@
 
 
 def validUTF8(data):
-    """
-    Method that determines if a given data set represents a valid UTF-8 encoding.
-    """
+    """ Method that determines if a given data set
+        represents a valid UTF-8 encoding. """
     n_bytes = 0
 
     for num in data:
