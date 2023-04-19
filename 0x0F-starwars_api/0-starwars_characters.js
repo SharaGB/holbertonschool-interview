d7dc9608724ce0478d6e4b31c9a49ff9ffc80400
SharaGB <shara.garcia.betancur@gmail.com>
2023-04-19 10:17:05 -0500

    Script that prints all characters of a Star Wars movie

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const request = require('request');
+request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, res, body) => {
+    if (!err) {
+        const characters = JSON.parse(body).characters;
+        recursionRequestPrint(characters, 0);
+    }
+});
+
+function recursionRequestPrint(url, index) {
+    request(url[index], (err, res, body) => {
+        if (!err) {
+            console.log(JSON.parse(body).name);
+            if (index + 1 < url.length) recursionRequestPrint(url, ++index);
+        }
+    });
+}
