5d383760f0bbc58c82e5d90994ba6e85a46541bd
SharaGB <shara.garcia.betancur@gmail.com>
2023-05-25 18:41:42 -0500

    Infinite Multiplication

---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,11 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int _putchar(char c);
+
+#endif /* _HOLBERTON_H_ */
