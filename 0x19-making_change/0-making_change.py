153d0f31eaedc4cec659b2dc8fb818ef7f65f25a
SharaGB <shara.garcia.betancur@gmail.com>
2023-07-24 20:49:59 -0500

    Determine the fewest number of coins needed to meet a given amount total

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,34 +1,41 @@
 #!/usr/bin/python3
 """
-Make change module
+Change comes from within
 """
 
 
 def makeChange(coins, total):
-    """Given a pile of coins of different values,
-    determine the fewest number of coins
-    needed to meet a given amount 'total'
+    """
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
 
-    Args:
-        coins ([list]): a list of the values of the coins in your possession
-        total ([number]): amount
     Return: fewest number of coins needed to meet total
+        - If total is 0 or less, return 0
+        - If total cannot be met by any number of coins you have, return -1
+
+        - Coins is a list of the values of the coins in your possession
+        - The value of a coin will always be an integer greater than 0
+        - You can assume you have an infinite number of each denomination of
+        coin in the list
     """
     if total <= 0:
         return 0
 
-    coins.sort(reverse=True)
+    placeholder = total + 1
 
-    i, ncoins = (0, 0)
-    cpy_total = total
-    len_coins = len(coins)
+    memo = {0: 0}
 
-    while(i < len_coins and cpy_total > 0):
-        if (cpy_total - coins[i]) >= 0:
-            cpy_total -= coins[i]
-            ncoins += 1
-        else:
-            i += 1
+    for i in range(1, total + 1):
+        memo[i] = placeholder
 
-    check = cpy_total > 0 and ncoins > 0
-    return -1 if check or ncoins == 0 else ncoins
+        for coin in coins:
+            current = i - coin
+            if current < 0:
+                continue
+
+            memo[i] = min(memo[current] + 1, memo[i])
+
+    if memo[total] == total + 1:
+        return -1
+
+    return memo[total]
