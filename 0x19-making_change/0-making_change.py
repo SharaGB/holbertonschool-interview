fb73bae6dec0640b04063c862aa76e58fb080d46
SharaGB <shara.garcia.betancur@gmail.com>
2023-07-24 20:38:51 -0500

    Determine the fewest number of coins needed to meet a given amount total

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
 """
-makeChange module
+Task: Change comes from within
+Given a pile of coins of different values,
+determine the fewest number of coins needed to
+meet a given amount total
 """
 
 
 def makeChange(coins, total):
     """
-    makeChange function
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+    Return: fewest number of coins needed to meet total
+        - If total is 0 or less, return 0
+        - If total cannot be met by any number of coins you have, return -1
+        - Coins is a list of the values of the coins in your possession
+        - The value of a coin will always be an integer greater than 0
+        - You can assume you have an infinite number of each denomination of
+        coin in the list
     """
     if total <= 0:
         return 0
