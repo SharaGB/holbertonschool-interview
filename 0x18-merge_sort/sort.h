b53b4abe0af42840a5706420ff4bed4115639cbf
SharaGB <shara.garcia.betancur@gmail.com>
2023-07-24 20:09:24 -0500

    Sorts an array of integers in ascending order using the Merge Sort algorithm

---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
+
+#endif
