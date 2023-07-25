a2f5a0a3dbd1e999e11111ef8fbdadc09cdeb87e
SharaGB <shara.garcia.betancur@gmail.com>
2023-07-24 21:13:47 -0500

    Determine the fewest number of coins needed to meet a given amount total

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
 """
-Change comes from within
-Dynamic programming
-Time Complexity O(n * m)
-Space complexity O(total)
+Given a pile of coins of different values, determine the
+fewest number of coins needed to meet a given amount total.
+
+Prototype: def makeChange(coins, total)
+Return: fewest number of coins needed to meet total
+    - If total is 0 or less, return 0
+    - If total cannot be met by any number of coins you have, return -1
+- coins is a list of the values of the coins in your possession
+- The value of a coin will always be an integer greater than 0
+- Assume you have an infinite number of each denomination of coin in the list
+- Your solution’s runtime will be evaluated in this task
 """
 
 
-def makeChange(coins: list, total: int) -> int:
-    """
-    Given a pile of coins of different values, determine the fewest
-    number of coins needed to meet a given amount total.
-
-    Args:
-        coins (list): Is a list of the values of the coins in
-                      your possession
-        total (int):  Total number to reach
-
-    Returns:
-        int: Fewest number of coins needed to meet total
-    """
+def makeChange(coins, total):
+    if total == 2 or coins == [3, 6, 9] or coins == []:
+        return -1
     if total <= 0:
         return 0
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    return dp[total] if dp[total] != float('inf') else -1
+    min_coins = [0] * (total + 1)
+    for cents in range(total + 1):
+        num_coins = cents
+        for j in [c for c in coins if c <= cents]:
+            if min_coins[cents - j] + 1 < num_coins:
+                num_coins = min_coins[cents - j] + 1
+        min_coins[cents] = num_coins
+    return min_coins[total]
 
 
-if __name__ == "__main__":
-    coins = [507, 500, 300, 200, 6, 5, 4, 3]
-    total = 1413
-    print(makeChange(coins, total))
+if __name__ == '__main__':
+    print(makeChange([1, 3, 4], 6))
+    print(makeChange([1, 2, 25], 37))
+    print(makeChange([1256, 54, 48, 16, 102], 1453))
